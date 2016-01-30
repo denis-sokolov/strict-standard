6ea0c60cfaf6515d2a720ba84a5908b192b8bd0e
Denis Sokolov <denis@sokolov.cc>
2016-01-30 16:00:35 +0200

    Upgrade to standard-engine v3.

--- src/options.js
+++ src/options.js
@@ -1,10 +1,12 @@
 'use strict'
 
+var eslint = require('eslint')
 var path = require('path')
 var pkg = require('../package.json')
 
 module.exports = {
   cmd: 'strict-standard',
+  eslint: eslint,
   version: pkg.version,
   homepage: pkg.homepage,
   bugs: pkg.bugs.url,
